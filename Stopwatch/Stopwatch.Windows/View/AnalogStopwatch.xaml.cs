05e9698149cef431cfcb7cf783bb7bf9005130b0
Rafal <rafal.araszkiewicz@student.put.poznan.pl>
2015-08-04 19:28:38 +0200

    Final version.

--- Stopwatch/Stopwatch.Windows/View/AnalogStopwatch.xaml.cs
+++ Stopwatch/Stopwatch.Windows/View/AnalogStopwatch.xaml.cs
@@ -10,8 +10,10 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Shapes;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -22,7 +24,27 @@ namespace Stopwatch.View
         public AnalogStopwatch()
         {
             InitializeComponent();
+            AddMarkings();
         }
+
+        private void AddMarkings()
+        {
+            for (int i = 0; i < 360; i+=3)
+            {
+                Rectangle rectangle = new Rectangle();
+                rectangle.Width = (i % 30 == 0) ? 3 : 1;
+                rectangle.Height = 15;
+                rectangle.Fill = new SolidColorBrush(Colors.Black);
+                rectangle.RenderTransformOrigin = new Point(0.5, 0.5);
+
+                TransformGroup transforms = new TransformGroup();
+                transforms.Children.Add(new TranslateTransform() { Y = -140 });
+                transforms.Children.Add(new RotateTransform() { Angle = i });
+                rectangle.RenderTransform = transforms;
+                baseGrid.Children.Add(rectangle);
+            }
+        }
+
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
             viewModel.Start();
